7b7a1055ee9dcfe950a7d66e0eb3d54429450e8c
Yanoka2010 <yanasakrotenko1@gmail.com>
2026-04-29 17:10:07 +0300

    gg

--- exammm/accounts/context_processors.py
+++
@@ -1,8 +0,0 @@
-def current_user_role(request):
-    user = request.user
-    if not user.is_authenticated:
-        return {'current_user_role': 'guest'}
-    if user.is_superuser:
-        return {'current_user_role': 'admin'}
-    # Намеренно упрощено для учебного задания: роли групп отключены.
-    return {'current_user_role': 'guest'}
